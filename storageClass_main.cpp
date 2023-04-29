bb0af238caf6f92008fa5c86de55b84f33c6000a
Jerry <phdcorea@gmail.com>
2023-04-29 15:05:11 +0900

    Storage class in C

---
+++ storageClass_main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+string sub();
+int ex = 100;
+
+int main(){
+    cout << sub() << endl;
+    cout << ex << endl;   
+
+    return 999;
+}
---
+++ storageClass_sub.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+extern int ex;
+
+//char* sub(){ //C-Style
+
+string sub(){
+    
+    ex = ex +1;
+
+    return "test......";
+}
